6c83804935865716f5702a5f76e8e6d4f18278e0
Mukhtar <mukkhtarr@gmail.com>
2020-11-22 16:15:13 -0800

    add customer form client and server side validation

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -26,6 +26,7 @@ namespace Vidly.Controllers
             
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
@@ -33,8 +34,20 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
+
             if(customer.Id == 0)
                 _context.Customers.Add(customer);
             else
